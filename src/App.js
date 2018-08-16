052ea9a188700e70cd6325fc40f150b37a0b1e1d
TrentLottery <trentlottery@protonmail.com>
2018-08-16 17:41:43 +0800

    second version

--- src/App.js
+++ src/App.js
@@ -7,6 +7,69 @@ import { Grid,Row,Col,Panel,Button,Table } from 'react-bootstrap';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      betRecord: [
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+        {
+          account: "fdklskkejsjh",
+          amount: "32135.2211",
+          bettime: "2017-09-22 12:21:45",
+        },
+      ],
+    }
+  }
+
+  renderBetRecords() {
+    return (
+      <Row style={{margin:"0"}}>
+        <Table responsive>
+          <tbody style={{border:"4px solid rgb(65, 117, 105)"}}>
+          {this.state.betRecord.map((rcd,index) => {
+            return (
+              <tr key={index}>
+                <td>{rcd.account}</td>
+                <td>{rcd.amount} EOS</td>
+                <td>{rcd.bettime}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </Table>
+      </Row>
+    )
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,64 +83,27 @@ class App extends Component {
         </p>
 
         <Grid>
-          <Row  className="per-title">
+          <Row  className="per-title order-list">
             <span>接盘记录</span>
             <Button bsStyle="danger" className="button-right">最低接盘价 34526.5346 EOS</Button>
           </Row>
 
-          <Row style={{margin:"0"}}>
-            <Table responsive>
-              <tbody style={{border:"1px solid #dddddd"}}>
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-
-                <tr>
-                  <td>fdklskkejsjh</td>
-                  <td>2377.5365 EOS</td>
-                  <td>2017-09-22 12:21:45</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Row>
+          {this.renderBetRecords()}
 
           <Row style={{margin:"0"}}>
-            <div className="per-title">游戏规则</div>
+            <div className="per-title" id="game-rule">游戏规则</div>
               <ul className="list-group rule-ul">
-                <li className="list-group-item">1、啊啊</li>
-                <li className="list-group-item">2、版本</li>
-                <li className="list-group-item">3、超出</li>
+                <li className="list-group-item">1、第一个成为stupid的人需要支付1个EOS，随后每一个完成支付的人最少需要是前一个人支付金额的1.1倍，上限为3倍；</li>
+                <li className="list-group-item">2、也就是说，如果完成支付后有其他用户来接盘，你将至少获得10%的利润</li>
+                <li className="list-group-item">3、用户可以根据当前形势决定自己要支付的金额，支付的越多风险越高，但是收益也越高；</li>
+                <li className="list-group-item">4、如果某用户支付12小时内没有人再来接盘，那么这个用户将成为Last Stupid，本轮游戏结束，进入下一轮；</li>
+                <li className="list-group-item">5、游戏过程中将收取5%的总费用，其中2%作为DAPP运营费用，3%作为资金池；</li>
+                <li className="list-group-item">6、每轮游戏的Last Stupid可以在后续游戏积累的资金池中分红，分红比例按照所有Last Stupid的个数平均分配，分红总量为资金池总额的70%；</li>
               </ul>
           </Row>
 
           <Row style={{margin:"0"}}>
-            <div className="per-title">历史记录</div>
+            <div className="per-title" id="history-records">历史记录</div>
             <Table className="history-ul">
               <thead>
                 <tr>
@@ -127,12 +153,12 @@ class App extends Component {
               </tbody>
             </Table>
             <div className="click-div">
-              <Button>
-                上一页
+              <Button className="prev">
+                &lt;&lt;
               </Button>
 
-              <Button>
-                下一页
+              <Button className="next">
+                >>
               </Button>
             </div>
           </Row>
--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends React.PureComponent {
       <Navbar collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Image src= "./img/logo.png" width="150px"  responsive/>
+            <Image src= "./img/logo.png"  />
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
@@ -23,13 +23,13 @@ export default class Header extends React.PureComponent {
 
           </Nav>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#">
+            <NavItem eventKey={1} className="li-a" href="#game-rule">
               游戏规则
             </NavItem>
-            <NavItem eventKey={2} href="#">
+            <NavItem eventKey={2} className="li-a" href="#history-records">
               历史记录
             </NavItem>
-            <NavDropdown eventKey={3} title="中文简体" id="basic-nav-dropdown">
+            <NavDropdown eventKey={3} className="li-a" title="中文简体" id="basic-nav-dropdown">
               <MenuItem eventKey={3.1}>中文简体</MenuItem>
               <MenuItem eventKey={3.2}>ENGLISH</MenuItem>
             </NavDropdown>
