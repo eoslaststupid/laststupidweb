05d9776ba448727cc18dfec1bec4c42b14452cab
TrentLottery <trentlottery@protonmail.com>
2018-08-17 11:48:06 +0800

    modify game rule

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
 import Top3banner from './Top3banner'
-import { Grid,Row,Col,Panel,Button,Table } from 'react-bootstrap';
+import { Grid,Row,Col,Panel,Button,Table,FormGroup,InputGroup,FormControl } from 'react-bootstrap';
 
 
 class App extends Component {
@@ -84,8 +84,27 @@ class App extends Component {
 
         <Grid>
           <Row  className="per-title order-list">
+            <Col xs={12} md={3}>
             <span>接盘记录</span>
-            <Button bsStyle="danger" className="button-right">最低接盘价 34526.5346 EOS</Button>
+            </Col>
+
+            <Col  xs={12} md={9}>
+              <Col xs={0} md={5}>
+              </Col>
+              <Col xs={12} md={7}>
+
+
+                <FormGroup>
+                  <InputGroup className="inputGroup">
+                    <FormControl type="text" name="betprice" placeholder="最低接盘价 34526.5346 EOS"/>
+                    <InputGroup.Button>
+                      <Button bsStyle="danger" >我来接盘</Button>
+                    </InputGroup.Button>
+                  </InputGroup>
+                </FormGroup>
+              </Col>
+
+            </Col>
           </Row>
 
           {this.renderBetRecords()}
@@ -93,18 +112,19 @@ class App extends Component {
           <Row style={{margin:"0"}}>
             <div className="per-title" id="game-rule">游戏规则</div>
               <ul className="list-group rule-ul">
-                <li className="list-group-item">1、第一个成为stupid的人需要支付1个EOS，随后每一个完成支付的人最少需要是前一个人支付金额的1.1倍，上限为3倍；</li>
-                <li className="list-group-item">2、也就是说，如果完成支付后有其他用户来接盘，你将至少获得10%的利润</li>
+                <li className="list-group-item">1、第一个成为stupid的人需要支付1个EOS，随后每一个接盘侠最少需要支付前一个人接盘金额的1.1倍，也可以选择支付更多但上限为3倍；</li>
+                <li className="list-group-item">2、接盘的资金将进入上一个玩家的钱包，所以一旦有其他玩家来接盘，你将至少获得10%的利润，除去手续费；</li>
                 <li className="list-group-item">3、用户可以根据当前形势决定自己要支付的金额，支付的越多风险越高，但是收益也越高；</li>
-                <li className="list-group-item">4、如果某用户支付12小时内没有人再来接盘，那么这个用户将成为Last Stupid，本轮游戏结束，进入下一轮；</li>
+                <li className="list-group-item">4、如果某玩家接盘后超过12小时没有人再来接盘，那么此玩家将成为Last Stupid，本轮游戏结束，进入下一轮；</li>
                 <li className="list-group-item">5、游戏过程中将收取5%的总费用，其中2%作为DAPP运营费用，3%作为资金池；</li>
-                <li className="list-group-item">6、每轮游戏的Last Stupid可以在后续游戏积累的资金池中分红，分红比例按照所有Last Stupid的个数平均分配，分红总量为资金池总额的70%；</li>
+                <li className="list-group-item">6、每轮游戏的Last Stupid可以在后续游戏积累的资金池中享受永久分红，分红比例按照所有Last Stupid的个数平均分配；</li>
+                <li className="list-group-item">7、分红总量为资金池总额的70%，剩余资金将积累到下一轮；</li>
               </ul>
           </Row>
 
           <Row style={{margin:"0"}}>
             <div className="per-title" id="history-records">历史记录</div>
-            <Table className="history-ul">
+            <Table className="history-ul" responsive>
               <thead>
                 <tr>
                   <th>轮次</th>
--- src/Header.js
+++ src/Header.js
@@ -16,12 +16,7 @@ export default class Header extends React.PureComponent {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              <strong  style={{fontSize: "2rem"}}>Last Stupid</strong>
-            </NavItem>
 
-          </Nav>
           <Nav pullRight>
             <NavItem eventKey={1} className="li-a" href="#game-rule">
               游戏规则
@@ -29,10 +24,9 @@ export default class Header extends React.PureComponent {
             <NavItem eventKey={2} className="li-a" href="#history-records">
               历史记录
             </NavItem>
-            <NavDropdown eventKey={3} className="li-a" title="中文简体" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>中文简体</MenuItem>
-              <MenuItem eventKey={3.2}>ENGLISH</MenuItem>
-            </NavDropdown>
+            <NavItem eventKey={2} className="li-a change-lan" href="#">
+              English
+            </NavItem>
 
           </Nav>
         </Navbar.Collapse>
